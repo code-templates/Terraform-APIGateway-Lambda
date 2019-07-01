b0d43d0f4cfdc6301aeffd3e3c432d8bbfbb4847
Baiyuan Wang <bywang@thoughtworks.com>
2019-07-01 17:34:02 +0800

    Update readme

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,15 +1,7 @@
 resource "aws_lambda_function" "example" {
   function_name = "ServerlessExample"
-
-  # The bucket name as created earlier with "aws s3api create-bucket"
-  # s3_bucket = "terraform-serverless-example"
-  # s3_key    = "v1.0.0/example.zip"
-
-  # "main" is the filename within the zip file (main.js) and "handler"
-  # is the name of the property under which the handler function was
-  # exported in that file.
-
 	filename         = "../lambda-code/example.zip"
+  source_code_hash = "${base64sha256(file("../lambda-code/example.zip"))}"
   handler = "main.handler"
   runtime = "nodejs10.x"
 
