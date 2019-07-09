438e796713eaa233cc09a7b538a2f982634840db
Baiyuan Wang <bywang@thoughtworks.com>
2019-07-09 21:31:46 +0800

    Add log support

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,6 +6,7 @@ resource "aws_lambda_function" "example" {
   runtime = "nodejs10.x"
 
   role = "${aws_iam_role.lambda_exec.arn}"
+  depends_on    = ["aws_iam_role_policy_attachment.lambda_logs", "aws_cloudwatch_log_group.example"]
 }
 
 # IAM role which dictates what other AWS services the Lambda function
@@ -41,3 +42,42 @@ resource "aws_lambda_permission" "apigw" {
   source_arn = "${aws_api_gateway_deployment.example.execution_arn}/*/*"
 }
 
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.example.arn}"
+  principal     = "logs.us-west-2.amazonaws.com"
+}
+
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/ServerlessExample"
+  retention_in_days = 14
+}
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+resource "aws_iam_policy" "lambda_logging" {
+  name = "lambda_logging"
+  path = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role = "${aws_iam_role.lambda_exec.name}"
+  policy_arn = "${aws_iam_policy.lambda_logging.arn}"
+}
